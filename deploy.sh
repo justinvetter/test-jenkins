08355f5248e9584016a47fa04b7538ba839a4417
Justin Vetter <justin.vetter@digitalglobe.com>
2017-04-19 15:10:23 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ REV=`git rev-parse HEAD` # Gets commit hash as message
 TAG=`git tag -l | tail -n1`
 git checkout production # Step 3
 git rm -rf . # Step 4
-git checkout gulp_production -- .gitignore # Step 5
+git checkout mater -- .gitignore # Step 5
 mv dist/* .
 rm -rf dist # Step 6
 git add . # Step 7
 git commit -m "deployed $TAG at $REV" # Step 8
 git push --all # Step 9
 git push live production # Step 10
-git checkout gulp_production # Step 11
+git checkout mastern # Step 11
 git stash pop # Applies previously saved stash so you can continue working on changes. Once applied, removes stash
