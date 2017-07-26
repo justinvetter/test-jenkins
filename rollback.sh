2ede161a68ec68fd44b29a0e1c0dcb6175c96352
Justin Vetter <justin.vetter@digitalglobe.com>
2017-07-26 14:35:58 -0600

    update rollback script save post-receive hook

--- rollback.sh
+++ rollback.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
+ssh justin.vetter@10.84.140.15 -i ~/.ssh/appsdg <<'ENDSSH'
 set -e
 RELEASES=4
 BLUE='\033[0;34m'
 NC='\033[0m'
-HOME=~/dev/test-jenkins
-CURRENT=$(ls -tp $HOME/build/releases | head -n 1)
-BACK=$(ls -tp $HOME/build/releases | tail -n 2 | head -n 1)
+HOME="/var/www/microsites/html/build-test"
+CURRENT=$(ls -tp $HOME/releases | head -n 1)
+BACK=$(ls -tp $HOME/releases | tail -n 2 | head -n 1)
 echo "${NC}Rolling back ${BLUE}$CURRENT ${NC}to ${BLUE}$BACK"
-ln -sfn "$HOME/build/releases/$BACK" "$HOME/build/html"
+ln -sfn "$HOME/releases/$BACK" "$HOME/html"
 echo "${NC}Removing ${BLUE}$CURRENT"
-cd $HOME/build/releases
+cd $HOME/releases
 rm -rf $CURRENT
+ENDSSH
